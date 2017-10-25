e9574299b6df5dfb23373383efb116bbf1c9e531
Szymon Miks <miks.szymon@gmail.com>
2017-10-25 18:03:49 +0200

    dodanie przykladu na przeciazanie i nadpisanie funkcji/metody

--- CPPBasic/src/Liczba.h
+++ CPPBasic/src/Liczba.h
@@ -2,6 +2,7 @@
 #define Liczba_h
 
 #include <iostream>
+#include <string>
 #include "WartoscLiczbowa.h"
 
 class Liczba : public WartoscLiczbowa
@@ -18,6 +19,15 @@ public:
 
 	void wypisz(std::ostream &out) {
 		out << *this;
+	}
+
+//	void test() override {}; // przyk³ad pokazuj¹cy, ¿e je¿eli w klasie bazowej nie ma funkcji test to kompilator da b³¹d
+	void func(int) {
+		std::cout << "int \n";
+	}
+
+	void func(std::string) {
+		std::cout << "string \n";
 	}
 private:
 	double re; //czesc rzeczywista
