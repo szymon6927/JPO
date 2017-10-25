3baec6503223d8710e08a5c1eb26f5be472f6310
Szymon Miks <miks.szymon@gmail.com>
2017-10-25 18:06:13 +0200

    utworzenie projektu na zadanie cppadv

---
+++ src/CPPAdv.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CPPAdv.cpp
+// Author      : Szymon Miks
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "JPO" << endl; // prints JPO
+	return 0;
+}
