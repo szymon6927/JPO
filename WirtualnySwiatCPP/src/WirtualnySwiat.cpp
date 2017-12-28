3441a60743cddcaec3cfc542cea64b4f3c7d0e91
Szymon Miks <miks.szymon@gmail.com>
2017-12-28 17:18:57 +0100

    utworzenie katlogu pod porjekt na laboratoria otwarte

---
+++ WirtualnySwiatCPP/src/WirtualnySwiat.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : WirtualnySwiat.cpp
+// Author      : Szymon Miks
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	std::cout << "Wirtualny swiat" << std::endl;
+	std::cin.get();
+	return 0;
+}
