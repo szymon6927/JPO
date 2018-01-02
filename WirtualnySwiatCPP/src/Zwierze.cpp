2627ca1d75e4011cc89badd396a1b2862a2683d4
Szymon Miks <miks.szymon@gmail.com>
2018-01-02 19:55:40 +0100

    usuniecie warunku na ogluszenie

--- WirtualnySwiatCPP/src/Zwierze.cpp
+++ WirtualnySwiatCPP/src/Zwierze.cpp
@@ -15,66 +15,51 @@ Zwierze::~Zwierze() {
 
 void Zwierze::akcja() {
     //zwykle poruszanie sie
+	int kierunek = rand() % 4;
+	const int wymiar = 20;
 
-    if (ogluszenie == 0) {
+	int tmp_x = x;
+	int tmp_y = y;
 
-        int kierunek = rand() % 4;
-        const int wymiar = 20;
+	if ((kierunek == 0) && (y > 0)) {
+		//ruch w gore
+		tmp_y -= 1;
+	}
+	else if ((kierunek == 1) && (x < wymiar - 1)) {
+		//ruch w prawo
+		tmp_x += 1;
+	}
+	else if ((kierunek == 2) && (y < wymiar - 1)) {
+		//ruch w dol
+		tmp_y += 1;
+	}
+	else if ((kierunek == 3) && (x > 0)) {
+		//ruch w lewo
+		tmp_x -= 1;
+	}
 
-        int tmp_x = x;
-        int tmp_y = y;
-
-        if ((kierunek == 0) && (y > 0)) {
-            //ruch w gore
-            tmp_y -= 1;
-        }
-        else if ((kierunek == 1) && (x < wymiar - 1)) {
-            //ruch w prawo
-            tmp_x += 1;
-        }
-        else if ((kierunek == 2) && (y < wymiar - 1)) {
-            //ruch w dol
-            tmp_y += 1;
-        }
-        else if ((kierunek == 3) && (x > 0)) {
-            //ruch w lewo
-            tmp_x -= 1;
-        }
-
-        if (zolwniety) {
-            zolwniety = false;
-        }
-
-        if (naMapie[tmp_x][tmp_y] == nullptr) {
-            naMapie[tmp_x][tmp_y] = naMapie[x][y];
-            naMapie[x][y] = nullptr;
-            x = tmp_x;
-            y = tmp_y;
-        }
-        else if (naMapie[tmp_x][tmp_y]->getSymbol() == symbol &&
-                 naMapie[tmp_x][tmp_y] != &*this) {
-            rozmnazanie(naMapie[tmp_x][tmp_y]); //sprawdz to
-        }
-        else if (naMapie[tmp_x][tmp_y] != &*this) {
-            naMapie[tmp_x][tmp_y]->kolizja(&*this);
-            kolizja(naMapie[tmp_x][tmp_y]);
-            if (isAlive() && !zolwniety) {
-                naMapie[tmp_x][tmp_y] = naMapie[x][y];
-                naMapie[x][y] = nullptr;
-                x = tmp_x;
-                y = tmp_y;
-            }
-            else if (!isAlive()) {
-                naMapie[x][y] = nullptr;
-            }
-        }
-    }
-    else {
-        ogluszenie--;
-    }
-    if (zolwniety) {
-        zolwniety = false;
-    }
+	if (naMapie[tmp_x][tmp_y] == nullptr) {
+		naMapie[tmp_x][tmp_y] = naMapie[x][y];
+		naMapie[x][y] = nullptr;
+		x = tmp_x;
+		y = tmp_y;
+	}
+	else if (naMapie[tmp_x][tmp_y]->getSymbol() == symbol && naMapie[tmp_x][tmp_y] != &*this) {
+		rozmnazanie(naMapie[tmp_x][tmp_y]); //sprawdz to
+	}
+	else if (naMapie[tmp_x][tmp_y] != &*this) {
+		naMapie[tmp_x][tmp_y]->kolizja(&*this);
+		kolizja(naMapie[tmp_x][tmp_y]);
+		if ( isAlive() ) {
+			naMapie[tmp_x][tmp_y] = naMapie[x][y];
+			naMapie[x][y] = nullptr;
+			x = tmp_x;
+			y = tmp_y;
+		}
+		else if (!isAlive()) {
+			naMapie[x][y] = nullptr;
+		}
+	}
 }
 
 void Zwierze::kolizja(organizm::Organizm* oponent) {
