d3ce7f61cc0cdf8f786af146852fb6938097de2c
Szymon Miks <miks.szymon@gmail.com>
2018-01-04 23:31:42 +0100

    ?

--- WirtualnySwiatCPP/src/Swiat.h
+++ WirtualnySwiatCPP/src/Swiat.h
@@ -10,29 +10,30 @@
 #include <vector>
 #include <algorithm>
 #include <string>
+#include "Organizm.h"
 
 #ifndef SWIAT_H_
 #define SWIAT_H_
 
 namespace swiat {
-class Swiat {
-    private:
-        static bool sortowanie(organizm::Organizm* i, organizm::Organizm* j);
-        static bool sortowanieZywych(organizm::Organizm* i, organizm::Organizm* j);
-    public:
-        organizm::Organizm*** mapaOrganizmow;
-        std::vector<organizm::Organizm*> organizmy;
-        std::vector<std::string> logger;
 
-        char** mapa;
-        void wykonajTure();
-        void rysujSwiat();
-        void utworzSwiat();
-        std::string zachowajSwiat();
-        Swiat();
-        ~Swiat();
+class Swiat {
+private:
+	static bool sortowanie(organizm::Organizm* i, organizm::Organizm* j);
+	static bool sortowanieZywych(organizm::Organizm* i, organizm::Organizm* j);
+public:
+	organizm::Organizm*** mapaOrganizmow;
+	std::vector<organizm::Organizm*> organizmy;
+	std::vector<std::string> logger;
+
+	char** mapa;
+	void wykonajTure();
+	void rysujSwiat();
+	void utworzSwiat();
+	std::string zachowajSwiat();
+	Swiat();
+	~Swiat();
 };
 }
-
 
 #endif /* SWIAT_H_ */
